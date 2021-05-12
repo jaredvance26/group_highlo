96ed5614fe1856b535f87084acf763a7c6874ac5
Brandon Densmore <densmoreb@Brandons-MacBook-Pro.local>
2021-05-12 15:45:37 -0600

    created files

---
+++ hilo/game/director.py
@@ -0,0 +1,14 @@
+from game.dealer import dealer
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to keep track of the score and control the 
+    sequence of play.
+    
+    Attributes:
+        keep_playing (boolean): Whether or not the player wants to keep playing.
+        score (number): The total number of points earned.
+        dealer (dealer): An instance of the class of objects known as dealer.
+    """
+
+    
